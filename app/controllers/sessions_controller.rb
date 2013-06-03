414353ab171635f57c3c8716da2b1356d0bfac31
Abby Raskin <abbyraskin@gmail.com>
2013-06-03 16:57:54 -0500

    login not case sensitive

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
 
-     @user = User.find_by_email(params[:email])
+     @user = User.find_by_email(params[:email].downcase)
 
     if @user.present? && @user.authenticate(params[:password])
       session[:email] = @user.email
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :email, :password, :password_confirmation
   validates :email, :uniqueness => true
-
   validates :email, :presence => true
   validates :first_name, :presence => true
   validates :last_name, :presence => true
@@ -10,5 +9,10 @@ class User < ActiveRecord::Base
 
   belongs_to :account
 
+  before_create :downcase_email
+
+  def downcase_email
+  self.email = self.email.downcase
+  end
 
 end
